7ea745ab6798f4b60683dbb476a02f295a8c6857
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-09-10 14:26:53 -0400

    Move doxygen files to dedicated folder

--- docs/search/pages_2.js
+++ docs/search/pages_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['frequently_20asked_20questions',['Frequently Asked Questions',['../md__f_a_q.html',1,'']]],
+  ['frequently_20asked_20questions',['Frequently Asked Questions',['../md_doxysetup__f_a_q.html',1,'']]],
   ['framework',['Framework',['../md__framework__r_e_a_d_m_e.html',1,'']]]
 ];
