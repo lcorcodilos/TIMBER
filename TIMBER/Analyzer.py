dff1f2117650c59f8e5e6a96170d777d3bfa990b
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-09-24 01:50:11 -0400

    Move CommonCscripts to common.h

--- TIMBER/Analyzer.py
+++ TIMBER/Analyzer.py
@@ -809,59 +809,6 @@ class Node(object):
                # column_vec.push_back(c)
             self.DataFrame.Snapshot(treename,outfilename,column_vec,opts)
 
-###############################
-# C script processing classes #
-###############################
-class CommonCscripts(object):
-    """Common c scripts all in analyzer namespace"""
-    def __init__(self):
-        super(CommonCscripts, self).__init__()
-        self.VecOps = 'using namespace ROOT::VecOps;'
-
-        self.deltaPhi ='''
-        namespace analyzer {
-          double deltaPhi(double phi1,double phi2) {
-            double result = phi1 - phi2;
-            while (result > TMath::Pi()) result -= 2*TMath::Pi();
-            while (result <= -TMath::Pi()) result += 2*TMath::Pi();
-            return result;
-          }
-        }
-        '''
-        self.vector = '''
-        namespace analyzer {
-            ROOT::Math::PtEtaPhiMVector TLvector(float pt,float eta,float phi,float m) {
-                ROOT::Math::PtEtaPhiMVector v(pt,eta,phi,m);
-                return v;
-            }
-        }
-        '''
-        self.invariantMass = '''
-        namespace analyzer {
-            double invariantMass(ROOT::Math::PtEtaPhiMVector v1, ROOT::Math::PtEtaPhiMVector v2) {
-                return (v1+v2).M();
-            }
-        }
-        '''
-        self.invariantMassThree = '''
-        namespace analyzer {
-            double invariantMassThree(ROOT::Math::PtEtaPhiMVector v1, ROOT::Math::PtEtaPhiMVector v2, ROOT::Math::PtEtaPhiMVector v3) {
-                return (v1+v2+v3).M();
-            }
-        }
-        '''
-        self.HT = '''
-        namespace analyzer {
-            float HT(std::vector<int> v) {
-                float ht = 0.0;
-                for(int pt : v) {
-                    ht = ht + pt
-                }
-                return ht;
-            }
-        }
-        '''
-        
 ##############################
 # Group class and subclasses #
 ##############################
---
+++ TIMBER/Framework/common.h
@@ -0,0 +1,47 @@
+#include <ROOT/RVec.hxx>
+
+using namespace ROOT::VecOps;
+namespace analyzer {   
+    std::vector<float> HistLookup(TH1D* hist, float xval, float yval=0.0, float zval=0.0){
+        std::vector<float> out;
+
+        int bin = hist->FindBin(xval,yval,zval); 
+        float Weight = hist->GetBinContent(bin);
+        float Weightup = Weight + hist->GetBinErrorUp(bin);
+        float Weightdown = Weight - hist->GetBinErrorLow(bin);
+        
+        out.push_back(Weight);
+        out.push_back(Weightup);
+        out.push_back(Weightdown);
+        return out;
+    }
+
+    float deltaPhi(float phi1,float phi2) {
+        float result = phi1 - phi2;
+        while (result > TMath::Pi()) result -= 2*TMath::Pi();
+        while (result <= -TMath::Pi()) result += 2*TMath::Pi();
+        return result;
+    }
+
+    ROOT::Math::PtEtaPhiMVector TLvector(float pt,float eta,float phi,float m) {
+        ROOT::Math::PtEtaPhiMVector v(pt,eta,phi,m);
+        return v;
+    }
+
+    double invariantMass(ROOT::Math::PtEtaPhiMVector v1, ROOT::Math::PtEtaPhiMVector v2) {
+        return (v1+v2).M();
+    }
+
+    double invariantMassThree(ROOT::Math::PtEtaPhiMVector v1, ROOT::Math::PtEtaPhiMVector v2, ROOT::Math::PtEtaPhiMVector v3) {
+        return (v1+v2+v3).M();
+    }
+
+    float HT(std::vector<int> pts) {
+        float ht = 0.0;
+        for(int pt : pts) {
+            ht = ht + pt;
+        }
+        return ht;
+    }
+
+}
