581c5e5b86c816022d5633ed98d86e14c1dac719
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-10-25 23:31:05 -0400

    Add new tests

--- test/test_Analyzer.py
+++ test/test_Analyzer.py
@@ -1,6 +1,6 @@
-import ROOT
+import ROOT, os
 ROOT.gROOT.SetBatch(True)
-from TIMBER.Analyzer import analyzer, CutGroup, VarGroup
+from TIMBER.Analyzer import *
 from TIMBER.Tools.Common import CompileCpp
 
 class TestAnalyzer():
@@ -8,6 +8,7 @@ class TestAnalyzer():
     def setup_class(cls):
         CompileCpp('TIMBER/Framework/include/common.h') # Compile (via gInterpreter) commonly used c++ code
         CompileCpp('examples/example.cc')
+        # CompileCpp('test/test_weight.cc')
         cls.a = analyzer('examples/GluGluToHToTauTau.root')
         cls.a.Cut("test_cut1", "Jet_pt.size() > 0")
         cls.a.Define('lead_vector', 'analyzer::TLvector(Jet_pt[0],Jet_eta[0],Jet_phi[0],Jet_mass[0])')
@@ -38,22 +39,47 @@ class TestAnalyzer():
         self.a.GetActiveNode().Snapshot(out_vars,str(tmp_path)+'/ex1_out.root','test_snapshot',lazy=False,openOption='RECREATE') 
         assert True
 
-    def test_AddCorrection(self):
+    def test_Correction(self):
+        c = Correction('testWeight','test/test_weight.cc')
+        self.a.Define('Jet_pt0','Jet_pt[0]')
+        self.a.AddCorrection(c,['Jet_pt0'])
+        self.a.MakeWeightCols()
+        htemplate = ROOT.TH1F('th1','',100,0,1000)
+        hgroup = self.a.MakeTemplateHistos(htemplate,'Jet_pt0')
+        print ([hgroup[h].GetName() for h in hgroup.keys()])
+        self.a.DrawTemplates(hgroup, './')
         pass
 
-    def test_MakeWeightCols(self):
+    def test_CommonVars(self):
+        assert self.a.CommonVars(["Muon","Tau"]) == ['phi', 'pt', 'charge', 'eta', 'mass', 'genPartIdx', 'jetIdx']
         pass
 
-    def test_MakeTemplateHistos(self):
+    def test_MergeCollections(self):
+        self.a.MergeCollections('Lepton',["Muon","Tau"])
         pass
 
-    def test_DrawTemplates(self):
+    def test_SubCollection(self):
+        self.a.SubCollection('Jet30','Jet','Jet_pt>30')
         pass
 
     def test_Nminus1(self):
+        cgroup = CutGroup('Nminus1Test')
+        cgroup.Add('cut1','Jet_eta[0] < 2.4 && Jet_eta[1] < 2.4')
+        cgroup.Add('cut2','All(Muon_pt < 30)')
+        cgroup.Add('cut3','Sum(Jet_btag > 0.5) > 1')
+        self.a.Nminus1(cgroup)
         pass
 
     def test_PrintNodeTree(self):
+        self.a.PrintNodeTree('test.pdf')
+        pass
+
+    def test_GetTriggerString(self):
+        assert self.a.GetTriggerString(['HLT_IsoMu24','HLT_IsoMu24_eta2p1','NotReal']) == '((HLT_IsoMu24==1) || (HLT_IsoMu24_eta2p1==1))'
+        pass
+
+    def test_GetFlagString(self):
+        assert self.a.GetFlagString(['HLT_IsoMu24','HLT_IsoMu24_eta2p1','NotReal']) == '((HLT_IsoMu24==1) && (HLT_IsoMu24_eta2p1==1))'
         pass
 
 def test_Groups():
