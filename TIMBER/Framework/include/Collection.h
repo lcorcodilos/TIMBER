bf5ed61e0c1f1b9abdd0cb6e281f83c72661ba6c
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-11-08 17:10:56 -0500

    Add Collection.h docstrings

--- TIMBER/Framework/include/Collection.h
+++ TIMBER/Framework/include/Collection.h
@@ -5,14 +5,15 @@
 using namespace ROOT::VecOps;
 using namespace std;
 
-/** Structure to store maps of the various types of objects
- * in a collection. UChar not considered.
+/** 
+ * @brief Structure to store maps of the various types of objects in a collection.
+ * UChar not considered.
  * Use by building each map as <branchName, branchValue> and
  * then assigning to the correct struct member. */
 struct Collection {
-    map<string,int*> Int;
-    map<string,bool*> Bool;
-    map<string,RVec<float>*> RVecFloat;
-    map<string,RVec<int>*> RVecInt;
+    map<string,int*> Int; /**< integer map*/
+    map<string,bool*> Bool; /**< bool map*/
+    map<string,RVec<float>*> RVecFloat; /**< RVec<float> map*/
+    map<string,RVec<int>*> RVecInt; /**< RVec<int> map*/
 };
 
