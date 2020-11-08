0a6483c262ba3e6cb02d84112b946f83b5af814e
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-11-08 17:00:42 -0500

    Add PDFweight_uncert docstrings

--- TIMBER/Framework/src/PDFweight_uncert.cc
+++ TIMBER/Framework/src/PDFweight_uncert.cc
@@ -7,19 +7,38 @@
 
 using namespace ROOT::VecOps;
 
-class PDFweight_uncert
-{
-private:
-    bool hessian;
-    bool ignoreEmpty;
-    int lhaid;
-public:
-    PDFweight_uncert(int lhaID, bool ignoreEmptyBranch = false);
-    ~PDFweight_uncert();
-    std::vector<float> eval(RVec<float> LHEPdfWeight);
+/**
+ * @class PDFweight_uncert
+ * @brief PDF weight uncertainty lookup class.
+ * 
+ * Accounts for hessians vs replicas by using LHA ID.
+ */
+class PDFweight_uncert {
+    private:
+        bool hessian;
+        bool ignoreEmpty;
+        int lhaid;
+    public:
+        /**
+         * @brief Construct a new PDF weight uncertainty lookup object
+         * 
+         * @param lhaID LHA ID for the PDF set used to determine whether
+         *  set is Hessian eigenvectors or MC replicas.
+         * @param ignoreEmptyBranch Ignores error that LHEPdfWeight branch is empty.
+         *  Defaults to false.
+         */
+        PDFweight_uncert(int lhaID, bool ignoreEmptyBranch = false);
+        ~PDFweight_uncert();
+        /**
+         * @brief Per-event evaluation function.
+         * 
+         * @param LHEPdfWeight The LHEPdfWeight branch/column.
+         * @return std::vector<float> {up, down} uncertainties in PDF weight.
+         */
+        std::vector<float> eval(RVec<float> LHEPdfWeight);
 };
 
-PDFweight_uncert::PDFweight_uncert(int lhaID, bool ignoreEmptyBranch){
+PDFweight_uncert::PDFweight_uncert(int lhaID, bool ignoreEmptyBranch = False){
     ignoreEmpty = ignoreEmptyBranch;
     lhaid = lhaID;
     // auto determine whether these are replicas or hessian eigenvectors
