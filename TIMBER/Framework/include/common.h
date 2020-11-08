75fcee2b422ae050e998a88e3e0f7bf35bf9ac9a
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-11-08 16:59:56 -0500

    Re-organize "Hardware" in common.h and add docstrings

--- TIMBER/Framework/include/common.h
+++ TIMBER/Framework/include/common.h
@@ -1,33 +1,46 @@
 #include <ROOT/RVec.hxx>
 
 using namespace ROOT::VecOps;
-namespace analyzer {   
-    std::vector<float> HistLookup(TH1D* hist, float xval, float yval=0.0, float zval=0.0){
-        std::vector<float> out;
-
-        int bin = hist->FindBin(xval,yval,zval); 
-        float Weight = hist->GetBinContent(bin);
-        float Weightup = Weight + hist->GetBinErrorUp(bin);
-        float Weightdown = Weight - hist->GetBinErrorLow(bin);
-        
-        out.push_back(Weight);
-        out.push_back(Weightup);
-        out.push_back(Weightdown);
-        return out;
-    }
-
-    float deltaPhi(float phi1,float phi2) {
+/**
+ * @namespace Hardware
+ * @brief Namespace for common physics functions.
+ */
+namespace Hardware {  
+    /**
+     * @brief Calculate the difference in \f$\phi\f$.
+     * 
+     * @param phi1 
+     * @param phi2 
+     * @return float Difference in \f$\phi\f$.
+     */
+    float DeltaPhi(float phi1,float phi2) {
         float result = phi1 - phi2;
         while (result > TMath::Pi()) result -= 2*TMath::Pi();
         while (result <= -TMath::Pi()) result += 2*TMath::Pi();
         return result;
     }
-
+    /**
+     * @brief Create a ROOT::Math::PtEtaPhiMVector.
+     * 
+     * @param pt 
+     * @param eta 
+     * @param phi 
+     * @param m 
+     * @return ROOT::Math::PtEtaPhiMVector 
+     */
     ROOT::Math::PtEtaPhiMVector TLvector(float pt,float eta,float phi,float m) {
         ROOT::Math::PtEtaPhiMVector v(pt,eta,phi,m);
         return v;
     }
-
+    /**
+     * @brief Create a vector of ROOT::Math::PtEtaPhiMVectors.
+     * 
+     * @param pt 
+     * @param eta 
+     * @param phi 
+     * @param m 
+     * @return RVec<ROOT::Math::PtEtaPhiMVector> 
+     */
     RVec<ROOT::Math::PtEtaPhiMVector> TLvector(RVec<float> pt,RVec<float> eta,RVec<float> phi,RVec<float> m) {
         RVec<ROOT::Math::PtEtaPhiMVector> vs;
         for (int i = 0; i < pt.size(); i++) {
@@ -37,30 +50,35 @@ namespace analyzer {
         return vs;
     }
 
+    /**
+     * @brief Calculate the transverse mass from MET \f$p_T\f$ and \f$\eta\f$
+     * and an object's \f$p_T\f$ and \f$\eta\f$.
+     * 
+     * @param MET_pt 
+     * @param obj_pt 
+     * @param MET_phi 
+     * @param obj_phi 
+     * @return float 
+     */
     float transverseMass(float MET_pt, float obj_pt, float MET_phi, float obj_phi) {
         return sqrt(2.0*MET_pt*obj_pt-(1-cos(deltaPhi(MET_phi,obj_phi))));
     }
 
-    double invariantMass(ROOT::Math::PtEtaPhiMVector v1, ROOT::Math::PtEtaPhiMVector v2) {
-        return (v1+v2).M();
-    }
-    double invariantMass(int idx1, int idx2, RVec<float> pts, RVec<float> etas, RVec<float> phis, RVec<float> masses) {
-        ROOT::Math::PtEtaPhiMVector v1, v2;
-        v1.SetCoordinates(pts[idx1],etas[idx1],phis[idx1],masses[idx1]);
-        v2.SetCoordinates(pts[idx2],etas[idx2],phis[idx2],masses[idx2]);
-        return invariantMass(v1,v2);
-    }
-
-    double invariantMassThree(ROOT::Math::PtEtaPhiMVector v1, ROOT::Math::PtEtaPhiMVector v2, ROOT::Math::PtEtaPhiMVector v3) {
-        return (v1+v2+v3).M();
-    }
-
-    float HT(std::vector<int> pts) {
-        float ht = 0.0;
-        for(int pt : pts) {
-            ht = ht + pt;
+    /**
+     * @brief Calculates the invariant mass of a vector of Lorentz vectors
+     * (ROOT::Math::PtEtaPhiMVector). Note that this is an alternative
+     * to [ROOT::VecOps::InvariantMasses()](https://root.cern/doc/master/namespaceROOT_1_1VecOps.html#a2c531eae910edad48bbf7319cc6d7e58)
+     * which does not need the intermediate Lorentz vector.
+     * 
+     * @param vects 
+     * @return double 
+     */
+    double invariantMass(RVec<ROOT::Math::PtEtaPhiMVector> vects) {
+        ROOT::Math::PtEtaPhiMVector sum;
+        sum.SetPtEtaPhiM(0,0,0,0);
+        for (int i = 0; i < vects.size(); i++) {
+            sum = sum + vects[i];
         }
-        return ht;
+        return sum.M();
     }
-
 }
