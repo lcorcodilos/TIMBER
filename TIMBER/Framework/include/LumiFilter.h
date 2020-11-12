8a539c38d622747bb11269bd12f5881598ba237e
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-11-12 14:57:43 -0500

    Add LumiFilter module, files, and docs

---
+++ TIMBER/Framework/include/LumiFilter.h
@@ -0,0 +1,74 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include "Pythonic.h"
+
+class LumiFilter {
+    private:
+        boost::property_tree::ptree ptree;
+        unsigned int lumStart;
+        unsigned int lumEnd;
+
+    public:
+        /**
+         * @brief Construct a new Lumi Filter object with a custom json file name
+         * 
+         * @param filename 
+         */
+        LumiFilter(std::string filename);
+        /**
+         * @brief Construct a new Lumi Filter object for a given year.
+         * Supports formatting as `2017` or `17`. 2016 not needed currently.
+         * 
+         * @param year 
+         */
+        LumiFilter(int year);
+        ~LumiFilter();
+        /**
+         * @brief Evaluate whether the given run and lumi pass the 
+         * luminosity JSON filter.
+         * 
+         * @param run 
+         * @param lumi 
+         * @return bool 
+         */
+        bool eval(unsigned int run, unsigned int lumi);
+};
+
+LumiFilter::LumiFilter(std::string filename) {
+    boost::property_tree::read_json(filename, ptree);
+}
+
+LumiFilter::LumiFilter(int year){
+    if (year == 17 or year == 2017) {
+        boost::property_tree::read_json(std::getenv("TIMBERPATH")+std::string("TIMBER/data/LumiJSON/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt"), ptree);
+    } else if (year == 18 or year == 2018) {
+        boost::property_tree::read_json(std::getenv("TIMBERPATH")+std::string("TIMBER/data/LumiJSON/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt"), ptree);
+    } else {
+        std::cout << "Year " << year << " not supported by LumiFilter" << std::endl;
+        throw;
+    }
+}
+
+LumiFilter::~LumiFilter() {
+}
+
+bool LumiFilter::eval(unsigned int run, unsigned int lumi) {
+    bool out = false;
+    for ( const auto& runEntry : ptree ) {
+        if (std::stoul(runEntry.first) == run) {
+            for ( const auto& lrEntry : runEntry.second ) {
+                const auto lrNd = lrEntry.second;
+                lumStart = std::stoul(lrNd.begin()->second.data());
+                lumEnd = std::stoul((++lrNd.begin())->second.data());
+                if ((lumi < lumEnd) && (lumi > lumStart)) {
+                    out = true;
+                }
+            }
+        }
+    }
+    return out;
+}
---
+++ TIMBER/data/README.md
@@ -0,0 +1,16 @@
+# Ledger of data files and their sources
+
+## Luminosity "golden" JSONs
+| Year | Date added | Source |
+|------|------------|--------|
+| 2017 | Nov 12, 2020 | [Twiki](https://twiki.cern.ch/twiki/bin/view/CMS/TWikiLUM#CurRec) |
+| 2018 | Nov 12, 2020 | [Twiki](https://twiki.cern.ch/twiki/bin/view/CMS/TWikiLUM#CurRec) |
+
+## Scale Factors
+
+| Correction | Group | Date added | File | JHUanalyzer module(s) | Sources |
+|------------|-------|------------|------|-----------------------|---------|
+| DeepCSV (2016 Legacy) | BTV | Jan 9, 2020   | DeepCSV_2016LegacySF_V1.csv        | None | [Twiki](https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation2016Legacy) |
+| DeepCSV (2017)        | BTV | Jan 9, 2020   | DeepCSV_94XSF_V4_B_F.csv           | None | [Twiki](https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation94X) |
+| DeepCSV (2018)        | BTV | Jan 9, 2020   | DeepCSV_102XSF_V1.csv              | None | [Twiki](https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation102X) |
+| Top tagging           | JME | Sept 22, 2020 | 201\*TopTaggingScaleFactors\*.root | None | [Twiki](https://twiki.cern.ch/twiki/bin/viewauth/CMS/JetTopTagging) [GitHub](https://github.com/cms-jet/TopTaggingScaleFactors) |
