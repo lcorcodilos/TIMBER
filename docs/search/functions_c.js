1a332a3bea67cb3d561d8e3d3f01c9f8ce4ccd6c
lcorcodilos <lac352@scarletmail.rutgers.edu>
2020-11-19 20:14:40 -0500

    Doc update from merge

--- docs/search/functions_c.js
+++ docs/search/functions_c.js
@@ -1,8 +1,10 @@
 var searchData=
 [
   ['makecall',['MakeCall',['../class_t_i_m_b_e_r_1_1_analyzer_1_1_correction.html#af3b819295400bd83f7131b6f0cb45aaa',1,'TIMBER::Analyzer::Correction']]],
+  ['makecumulative',['MakeCumulative',['../group___plot.html#ga48a04f428269184b1a9c2c07991b49b0',1,'TIMBER::Tools::Plot']]],
   ['makehistswithbinning',['MakeHistsWithBinning',['../class_t_i_m_b_e_r_1_1_analyzer_1_1analyzer.html#a033c29fbbeb320baadaec86ca25ff829',1,'TIMBER::Analyzer::analyzer']]],
   ['makepullplot',['MakePullPlot',['../group___plot.html#ga7c16fa7d4ea701ed0825f1b7dd9afb49',1,'TIMBER::Tools::Plot']]],
+  ['makesoverb',['MakeSoverB',['../group___plot.html#gaf2a2fbe25ad5ef71e42661b4210189e0',1,'TIMBER::Tools::Plot']]],
   ['maketemplatehistos',['MakeTemplateHistos',['../class_t_i_m_b_e_r_1_1_analyzer_1_1analyzer.html#a611faeda8b08b76181109820e549c232',1,'TIMBER::Analyzer::analyzer']]],
   ['makeupdown',['MakeUpDown',['../group___plot.html#ga49a8db027ed151ca43425510a1e6c9f7',1,'TIMBER::Tools::Plot']]],
   ['makeweightcols',['MakeWeightCols',['../class_t_i_m_b_e_r_1_1_analyzer_1_1analyzer.html#adea335ba0db85d4bf48562523fffe963',1,'TIMBER::Analyzer::analyzer']]],
