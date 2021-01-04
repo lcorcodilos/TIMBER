e704305e7f1a8a9eda7a273da1f45583d1658e40
lcorcodilos <lac352@scarletmail.rutgers.edu>
2021-01-04 13:39:05 -0500

    Add eval_bybin and eval_byglobal (with 2D and 3D support)

--- TIMBER/Framework/src/EffLoader.cc
+++ TIMBER/Framework/src/EffLoader.cc
@@ -9,8 +9,14 @@
 class EffLoader {
     private:
         TFile *file;
-    public:
         TEfficiency *efficiency; //!< Histogram object
+        int binx;
+        int globalbin;
+        float effval;
+        float effup;
+        float effdown;
+
+    public:
         /**
          * @brief Empty constructor
          */
@@ -23,19 +29,30 @@ class EffLoader {
          */
         EffLoader(std::string filename, std::string histname);
         /**
-         * @brief Evaluate by bin numbers.
+         * @brief Evaluate by global bin number.
          * 
-         * @param binx 
+         * @param globalbin 
          * @return std::vector<float> {nominal value, up error+nominal, down error+nominal}
          */
-        std::vector<float> eval_bybin(int binx);
+        std::vector<float> eval_byglobal(int globalbin);
+        /**
+         * @brief Evaluate by per-axis bin numbers.
+         * 
+         * @param binx 
+         * @param biny
+         * @param binz 
+         * @return std::vector<float> {nominal value, up error+nominal, down error+nominal}
+         */
+        std::vector<float> eval_bybin(int binx, int biny = 0, int binz = 0);
         /**
          * @brief Evaluate by axis value.
          * 
          * @param xval 
+         * @param yval
+         * @param zval
          * @return std::vector<float> {nominal value, up error+nominal, down error+nominal}
          */
-        std::vector<float> eval(float xval);
+        std::vector<float> eval(float xval, float yval = 0, float zval = 0);
             
 };
 
@@ -44,16 +61,19 @@ EffLoader::EffLoader(std::string filename, std::string effname) {
     efficiency = (TEfficiency*)file->Get(effname.c_str());
 }
 
-
-std::vector<float> EffLoader::eval(float xval){
-    int binx;
-    float effval;
-    float effup;
-    float effdown;
-    binx    = efficiency->FindFixBin(xval);
-    effval  = efficiency->GetEfficiency(binx);
-    effup   = effval + efficiency->GetEfficiencyErrorUp(binx);
-    effdown = effval - efficiency->GetEfficiencyErrorLow(binx);
+std::vector<float> EffLoader::eval_byglobal(int globalbin){
+    effval  = efficiency->GetEfficiency(globalbin);
+    effup   = effval + efficiency->GetEfficiencyErrorUp(globalbin);
+    effdown = effval - efficiency->GetEfficiencyErrorLow(globalbin);
     return {effval,effup,effdown};
 }
 
+std::vector<float> EffLoader::eval_bybin(int binx, int biny, int binz){
+    globalbin = efficiency->GetGlobalBin(binx, biny, binz);
+    return eval_byglobal(globalbin);
+}
+
+std::vector<float> EffLoader::eval(float xval, float yval, float zval){
+    globalbin    = efficiency->FindFixBin(xval, yval, zval);
+    return eval_byglobal(globalbin);
+}
