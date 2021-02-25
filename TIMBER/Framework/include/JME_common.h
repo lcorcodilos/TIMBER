1e579d67abaae4dcdbbf65c160b392fb18e6e12c
Lucas Corcodilos <lcorcodi@cern.ch>
2021-02-24 21:47:25 -0600

    Add JME_common.h

---
+++ TIMBER/Framework/include/JME_common.h
@@ -0,0 +1,50 @@
+#include "CondFormats/JetMETObjects/interface/JetCorrectorParameters.h"
+#include "CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h"
+#include "common.h"
+
+using str = std::string;
+
+class JMEpaths {
+    private:
+        const str _globalTag, _jetFlavour;
+        const str _jmeArchivePath = "TIMBER/data/JME/";
+        TempDir tempdir;
+
+    public:
+        JMEpaths(str globalTag, str jetFlavour) :
+                _globalTag(globalTag), _jetFlavour(jetFlavour){
+
+        };
+        str GetLevel(str level){
+            str out;
+            if (level == "L1") {
+                out = "_L1FastJet_";
+            } else if (level == "L2") {
+                out = "_L2Relative_";
+            } else if (level == "L3") {
+                out = "_L3Absolute_";
+            } else if (level == "Res") {
+                out = "_L2L3Residual_";
+            } else if (level == "Uncert") {
+                out = "_Uncertainty_";
+            }
+
+            return out;
+        }
+        str GetPath(str level) {
+            str tarfile, jmefile, filestr, p;
+            
+            tarfile = _jmeArchivePath + _globalTag + ".tgz";
+            jmefile = _globalTag+this->GetLevel(level)+_jetFlavour+".txt";
+            filestr = ReadTarFile(tarfile, jmefile);
+            p = tempdir.Write(jmefile, filestr);
+            
+            return p;
+        }
+        JetCorrectorParameters GetParameters(str level) {
+            return JetCorrectorParameters(this->GetPath(level));
+        };
+        JetCorrectionUncertainty GetUncertainty() {
+            return JetCorrectionUncertainty(this->GetPath("Uncert"));
+        }
+};
