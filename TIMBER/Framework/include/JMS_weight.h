75c6481d6da35dc576e782ec5131fb2f7f397fe3
Lucas Corcodilos <lcorcodi@cern.ch>
2021-03-01 15:08:47 -0600

    JMS_weight jmsTable->_jmsTable

--- TIMBER/Framework/include/JMS_weight.h
+++ TIMBER/Framework/include/JMS_weight.h
@@ -3,10 +3,10 @@
 #include <map>
 #include <vector>
 
-//Class to handle JMS uncertainty shift
+// CURRENTLY ONLY SUPPORTS TAU21
 class JMS_weight {
     private:
-        std::map< int, std::vector<float> > jmsTable {
+        std::map< int, std::vector<float> > _jmsTable {
             {2016, {1.000, 1.0094, 0.9906} },
             {2017, {0.982, 0.986 , 0.978 } },
             {2018, {0.982, 0.986 , 0.978 } }
