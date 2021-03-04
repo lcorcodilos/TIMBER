0841685136f1a8e443e5c1f91aca200d6f163450
Lucas Corcodilos <lcorcodi@cern.ch>
2021-03-03 21:27:53 -0600

    Add JME tarballs

---
+++ TIMBER/data/JER/get.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+wget https://github.com/cms-jet/JRDatabase/raw/master/tarballs/$1 -O $1 
---
+++ TIMBER/data/JES/get.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+wget https://github.com/cms-jet/JECDatabase/raw/master/tarballs/$1 -O $1 
