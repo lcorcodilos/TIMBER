8d06bb546d81a3c3b3658084346eb30d73dd959b
lcorcodilos <lac352@scarletmail.rutgers.edu>
2021-04-05 12:46:45 -0400

    Turn HEM_drop into a class for Correction() compat

--- TIMBER/Framework/src/HEM_drop.cc
+++ TIMBER/Framework/src/HEM_drop.cc
@@ -1,28 +1,31 @@
 #include "../include/HEM_drop.h"
 
-float HEM_drop(std::string setname, RVec<float> FatJet_eta, RVec<float> FatJet_phi, int run, std::vector<int> idxToCheck) {
-    bool isAffectedData = InString("data",setname) && (InString("C",setname) || InString("D",setname) || InString("B",setname));
-    bool isDataB = InString("dataB",setname);
-    float eventWeight = 1.0;
-    if (idxToCheck.empty()) {
-        idxToCheck.resize(FatJet_eta.size());
-        std::iota(idxToCheck.begin(), idxToCheck.end(), 0);
-    }
+HEM_drop::HEM_drop(std::string setname, std::vector<int> idxToCheck) :
+    _isAffectedData(InString("data",setname) && (InString("C",setname) || InString("D",setname) || InString("B",setname))),
+    _isDataB(InString("dataB",setname)), _idxToCheck(idxToCheck), _setname(setname)
+{};
 
-    for (size_t i = 0; i < idxToCheck.size(); i++) {
-        int idx = idxToCheck[i];
+std::vector<float> HEM_drop::eval(RVec<float> FatJet_eta, RVec<float> FatJet_phi, int run) {
+    float eventWeight = 1.0;
+    if (_idxToCheck.empty()) {
+        _idxToCheck.resize(FatJet_eta.size());
+        std::iota(_idxToCheck.begin(), _idxToCheck.end(), 0);
+    }
+    for (size_t i = 0; i < _idxToCheck.size(); i++) {
+        int idx = _idxToCheck[i];
         if ((FatJet_eta[idx] < -1.3) && (FatJet_eta[idx] > -2.5) && (FatJet_phi[idx] < -0.87) && (FatJet_phi[idx] > -1.57)){
-            if (isAffectedData) {
-                if (!(isDataB && run<319077)) {
+            if (_isAffectedData) {
+                if (!(_isDataB && run<319077)) {
                     eventWeight = 0.0; // If affected 2018B, C, or D
                     break;
                 }
-            } else if (!InString("data",setname)){
+            } else if (!InString("data",_setname)){
                 eventWeight = 0.353; // If affected MC
                 break;
             }
         }
         // std::cout << eventWeight << std::endl;
     }
-    return eventWeight;
+    return {eventWeight};
+
 }
