c9aaefef6763f6e4e051d98c84c1b57d277059b7
Amitav Mitra <amitavm@bu.edu>
2021-04-13 13:43:06 -0400

    make setup operation clearer

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ fi
 
 if ! command -v dot &> /dev/null
 then
-  echo "dot (graphviz) could not be found. Please install it first... (on Ubuntu `sudo apt-get install graphviz libgraphviz-dev`)"
-  exit
+  echo "dot (graphviz) could not be found. Please install it first... (on Ubuntu 'sudo apt-get install graphviz libgraphviz-dev')"
+  return
 fi
 
 python setup.py install
